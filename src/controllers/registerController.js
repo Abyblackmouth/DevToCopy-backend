d755888f73946c64744a79fbc2c5b366919280f9
abyblackmouth <abraham.covarrubias@live.com>
2023-09-05 21:17:56 -0600

    create estructure register

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -1,15 +1,18 @@
+import bcrypt from "bcrypt"
 import User from "../models/User"
 
 export const register = async (request, response) =>{
 
     const {name, lastName, email, user, password} = request.body
 
+    const hashedPassword = await bcrypt.hash(password, 10)
+
     const newUser = new User ({
         name,
         lastName,
         email,
         user,
-        password
+        password: hashedPassword
     })
     await newUser.save()
     console.log(newUser)
